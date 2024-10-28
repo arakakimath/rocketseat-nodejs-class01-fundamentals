1a6f5888e436570790708f613ded7ef70498ab2d
Matheus Arakaki <arakakimath@gmail.com>
2024-10-28 19:27:44 -0400

    creating database with persist method

--- src/database/database.js
+++ src/database/database.js
@@ -1,5 +1,25 @@
+import fs from 'node:fs/promises'
+
+const databasePath = new URL('db.json', import.meta.url)
+
 export class Database {
   #database = {};
+
+  constructor() {
+    fs.readFile(databasePath, 'utf8')
+      .then(data => {
+        this.#database = JSON.parse(data)
+      })
+      .catch(() => {
+        this.#persist()
+      })
+  }
+
+  #persist = () => {
+    //fs.writeFile('db.json', JSON.stringify(this.#database))
+    //create db.json according to actual address in terminal
+    fs.writeFile(databasePath, JSON.stringify(this.#database))
+  }
 
   select = (table) => {
     const data = this.#database[table] ?? []; //returns [] if the arg is null/undefined
@@ -13,6 +33,8 @@ export class Database {
     } else {
       this.#database[table] = [data];
     }
+
+    this.#persist()
 
     return data;
   };
