1b6f5dbfc67ea0bca99a294c8c9ffed4515674f3
Matheus Arakaki <arakakimath@gmail.com>
2024-10-28 18:51:30 -0400

    setting stream buffers to catch the body from request

--- src/server.js
+++ src/server.js
@@ -4,18 +4,31 @@ import http from "node:http"; // Uses 'node:' to identify intern modules
 
 const users = []
 
-const server = http.createServer((req, res) => {
+const server = http.createServer(async (req, res) => {
   const { method, url } = req;
+
+  const buffers = [] //initial array that will catch all chunks from the req stream
+
+  for await (const chunk of req)
+    buffers.push(chunk)
+
+  try{
+    req.body = JSON.parse(Buffer.concat(buffers).toString())
+  } catch {
+    req.body = null
+  }
 
   if ((method === "GET") & (url === "/users"))
     return res.setHeader("Content-type", "application/json")
             .end(JSON.stringify(users))
   
   if ((method === "POST") & (url === "/users")){
+    const { name, email } = req.body
+
     users.push({
       id: 1,
-      name: "Ar Math",
-      email: "armath@email.com"
+      name,
+      email
     })
 
     return res.writeHead(201).end()
